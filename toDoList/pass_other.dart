ec75c0c4f1bd65c67c6de5ce706e65771c0ef199
ymatsumi-vc <ymatsumi@valuecommerce.co.jp>
2025-06-14 08:54:53 +0900

    todoリスト

---
+++ toDoList/pass_other.dart
@@ -0,0 +1,56 @@
+/// 取材パス＞取材パス
+/// lib/task/task_bottom_sheet/each_task/visiting/visiting_widget.dart#L324
+
+このText下に追加
+if (widget.match.invitation.hasPrReward())
+  Text(
+    'Text追加',
+    FlutterFlowTheme.of(context).bodyMedium.copyWith(),
+  ),
+
+/// やること＞投稿前ステータス
+/// lib/task/task_bottom_sheet/each_task/submit_sns_reviews/submit_sns_review_widget.dart#L1854C17-L1863C19
+
+/// Text()内容修正
+/// このText下に追加
+if (widget.match.invitation.hasPrReward())
+  Text(
+    'Text追加',
+    FlutterFlowTheme.of(context).bodyMedium.copyWith(),
+  ),
+
+/// やること＞投稿・レポート確認中ステータス
+/// lib/task/task_component/invitation_process_waiting/invitation_process_waiting_widget.dart#L74
+/// この下に追加
+if (widget.match.invitation.hasPrReward())
+  Padding(
+    padding: const EdgeInsets.only(top: 8.0),
+    child: Text(
+        'Text追加',
+        FlutterFlowTheme.of(context).bodyMedium.copyWith(),
+    ),
+  ),
+
+/// やること > インサイトデータ提出
+/// Text()修正
+/// この下に追加
+if (widget.match.invitation.hasPrReward())
+  Text(
+    'Text追加',
+    FlutterFlowTheme.of(context).bodyMedium.copyWith(),
+  ),
+
+/// やること＞イベント評価中ステータス
+/// lib/task/task_component/invitation_process_todo/invitation_process_todo_widget.dart#L96
+/// タスクの完了お疲れ様でした！\n\n{pr_reward}こちらの評価は現状BUZMA運営のみに開示されます。サービス品質向上のため、率直なご意見・ご感想をお聞かせください。
+/// これを文字列変換をする。
+Padding(
+  padding: ...,
+  child: Text(
+    "sample{pr_reward}test".replaceAll(
+      '{pr_reward}',
+      widget.match.invitation.hasPrReward()
+        ? '追加文字(\n込み)',
+        : '',
+  ),
+)
