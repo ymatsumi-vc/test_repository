e2a66c776129478351d93101458958c44cf05e0b
ymatsumi-vc <ymatsumi@valuecommerce.co.jp>
2025-08-28 10:29:09 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -83,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            print("これはテストですよ.");
+            print("これはテストですよ。");
             const Text(
               'You have pushed the button this many times:',
             ),
