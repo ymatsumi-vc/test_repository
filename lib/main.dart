0a5aea6fe793b6d7e2dfa5e4cc5e99a5fbe4e1b4
ymatsumi-vc <ymatsumi@valuecommerce.co.jp>
2025-08-28 11:24:32 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
